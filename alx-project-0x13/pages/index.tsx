6b1a8ccf3f7097b3e454f6fcd993aeafff716d0c
graciry <ngunyigwothaya@gmail.com>
2025-06-22 11:16:03 +0300

    add

--- alx-project-0x13/pages/index.tsx
+++ alx-project-0x13/pages/index.tsx
@@ -1,6 +1,5 @@
 import ImageCard from "@/components/common/ImageCard";
 import useFetchData from "@/hooks/useFetchData";
-import { ImageProps } from "@/interfaces";
 import React, { useEffect, useState } from "react";
 
 const Home: React.FC = () => {
